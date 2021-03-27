bb6807eeacda285ba68f27da3bd0a3cf2c6ae685
Ravi Krishna Reddy <ravikrishna.ch@outlook.com>
2021-03-28 00:14:47 +0530

    commit

--- Scratch Pad 03.py
+++ Scratch Pad 03.py
@@ -1,10 +1,5 @@
-import matplotlib.pyplot as plt
 import numpy as np
 
-x = np.linspace(0, 5, 11)
-y = x**2
+x = np.array(range(1,11))
+y = x^2
 
-plt.plot(x, y)
-plt.xlabel("X")
-plt.ylabel("Y")
-plt.title("X vs Y")
